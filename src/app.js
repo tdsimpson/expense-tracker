3af181085d95f73df734d6becaf2f1b1d63749fa
Taylor Simpson <tdsimpson23@gmail.com>
2019-11-15 00:39:51 -0500

    Commented out dummy expense data

--- src/app.js
+++ src/app.js
@@ -14,14 +14,18 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Gas Bill", createdAt: "1000" }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
+//DELETED IN 131. GIT INTERGRATION -----
+
+// store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
+// store.dispatch(addExpense({ description: "Gas Bill", createdAt: "1000" }));
+// store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// //console.log(visibleExpenses);
+
+///---------------------------------------
 
 const jsx = (
     <Provider store={store}>
