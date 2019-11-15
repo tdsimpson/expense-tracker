1dcaf9531f6c4bb4c4bb09d009af5f686ea614c8
Taylor Simpson <tdsimpson23@gmail.com>
2019-11-15 00:48:50 -0500

    Updating name in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>Budget App</h1>
+        <h1>Expense Tracker</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
