0ebfa7008b5776f3fc826a4779634378ec0f984a
Taylor Simpson <tdsimpson23@gmail.com>
2019-11-26 23:58:26 -0500

    Beginning to add the ExpensesSummary component

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+import expensesTotal from '../selectors/expenses-total'
+import expenses from '../selectors/expenses';
+
+
+const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? "expense" : "expenses";
+    const formattedExpensesTotal = numeral(expensesTotal / 100).format("$0,0.00");
+    return (
+        <div>
+            <h1>Viewing {expenseCount} {expenseWord} totaling {expensesTotal} </h1>
+        </div>
+    )
+}
+
+export default ExpensesSummary;
