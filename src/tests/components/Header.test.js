744ba0f11d218480c2884999d8b1cd0b7801fcc0
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-01 11:49:44 -0500

    Enzyme fully functional

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,10 +4,7 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').length).toBe(1);
-    // const renderer = new ReactShallowRender();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
 });
 
 
