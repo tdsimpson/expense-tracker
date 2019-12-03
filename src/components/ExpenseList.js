8c7feba2970284d873bc2e654be722911feab313
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-02 23:56:29 -0500

    Adding test for ExpenseListItem, NotFoundPage, and ExpenseDashboardPage

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,12 +3,18 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
         <h1> Expenses List</h1>
-        {props.expenses.map((expense) => {
-            return <ExpenseListItem key={expense.id} {...expense} />
-        })}
+        {
+            props.expenses.length === 0 ? (
+                <p>No expenses</p>
+            ) : (
+                    props.expenses.map((expense) => {
+                        return <ExpenseListItem key={expense.id} {...expense} />
+                    })
+                )
+        }
     </div>
 );
 
