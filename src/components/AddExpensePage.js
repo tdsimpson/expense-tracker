b82b8b64fa71ed28e05f2f2fdc4a1fb5c44ce445
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-20 14:50:20 -0500

    Adding tests for AddExpensePage and EditExpensePage

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { startAddExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
-        this.props.startAddExpense(expense);
+        this.props.addExpense(expense);
         this.props.history.push('/');
     };
     render() {
@@ -21,7 +20,7 @@ export class AddExpensePage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    startAddExpense: (expense) => dispatch(startAddExpense(expense))
+    addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
