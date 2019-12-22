f65387e7fc2140c962ae77598d8323af2d93201b
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-22 11:34:39 -0500

    Setup test database environment

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,4 +1,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
+DotEnv.config({ path: 'env.test' });
 Enzyme.configure({ adapter: new Adapter() });
+
