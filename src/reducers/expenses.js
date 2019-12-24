9644a4a9d0a65bec867d3c42051da007323f5e86
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-23 23:44:30 -0500

    Added SET_EXPENSES action and reducer with tests

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -18,6 +18,8 @@ export default (state = expensesReducerDefaultState, action) => {
                     return expense
                 }
             });
+        case "SET_EXPENSES":
+            return action.expenses;
         default:
             return state
     }
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import DotEnv from 'dotenv';
 
-DotEnv.config({ path: 'env.test' });
+require('dotenv').config({ path: '.env.test' });
+
 Enzyme.configure({ adapter: new Adapter() });
 
