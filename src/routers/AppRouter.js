a6e1cbab1d39e6e4e94341bac1b71003070a40e8
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-26 23:40:19 -0500

    Added login and logoout functionality

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,6 +6,7 @@ import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header'
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
+import LoginPage from '../components/LoginPage';
 
 
 const AppRouter = () => (
@@ -13,7 +14,8 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/" component={LoginPage} exact={true} />
+                <Route path="/dashboard" component={ExpenseDashboardPage} />
                 <Route path="/create" component={AddExpensePage} />
                 <Route path="/edit/:id" component={EditExpensePage} />
                 <Route path="/help" component={HelpPage} />
