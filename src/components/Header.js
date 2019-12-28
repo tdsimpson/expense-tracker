e5c67db473da97e80f3923e2285b0107cb58af72
Taylor Simpson <tdsimpson23@gmail.com>
2019-12-28 16:39:04 -0500

    Added styles for summary section

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export const Header = ({ startLogout }) => (
                 <Link className="header__title" to="/dashboard">
                     <h1>Expense Tracker</h1>
                 </Link>
-                <button onClick={startLogout}>Logout</button>
+                <button className="button button--link" onClick={startLogout}>Logout</button>
             </div>
         </div>
     </header>
